c0b2748c8fae7067f8ee911f3f07c2e3f93fe04c
wissem gharbi <gamerlolggg1@gmail.com>
2022-06-23 04:38:42 +0100

    Add project files.

---
+++ E/Entity.cs
@@ -0,0 +1,38 @@
+﻿using Taped.Core;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+namespace E
+{
+    //make a chest game at the end to demonstrate 
+
+    public class Entity : GameObject
+    {
+        public override void Start()
+        {
+            animator.LoadTexture("E.png", 1);
+            //AddToActiveScene();
+            base.Start();
+        }
+
+        public override void Update(double time, KeyboardState key)
+        {
+           
+                Entity2 a = new Entity2();
+            
+        }
+    }
+
+    public class Entity2 : GameObject
+    {
+        public override void Awake()
+        {
+            animator.LoadTexture("E.png", 1);
+            base.Awake();
+        }
+
+        public override void Update(double time, KeyboardState key)
+        {
+            transform.position += new OpenTK.Mathematics.Vector3(1, 0, 0) * 1000 * (float)time;
+            base.Update(time, key);
+        }
+    }
+}
